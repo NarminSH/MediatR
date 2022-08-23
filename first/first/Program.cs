2d9dcb168aef7866d5cf282302ec560f72116f57
NarminSH <nshivakhanova@gmail.com>
2022-08-23 19:16:48 +0400

    2nd task

--- first/first/Program.cs
+++ first/first/Program.cs
@@ -1,20 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] nums = { 10, 20, 30, 40, 20, 30};
+//1st task
+//int[] nums = { 10, 20, 30, 40, 20, 30};
 
-Dictionary<int, int> dict = new Dictionary<int, int>();
+//Dictionary<int, int> dict = new Dictionary<int, int>();
 
+//var temp = new HashSet<Int32>();
 
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (temp.Contains(nums[i]))
+//    {
+//        Console.WriteLine(nums[i] + " is a duplicate");
+//    }
+//    else
+//    {
+//        temp.Add(nums[i]);
+//    }
+//}
 
-var temp = new HashSet<Int32>();
+// 2nd task
 
-for (int i = 0; i < nums.Length; i++)
+static bool checkName()
 {
-    if (temp.Contains(nums[i]))
+    string name = "kaman";
+    string[] names = { "Narmin", "Leyla", "Arzu" };
+    for (int i = 0; i < names.Length; i++)
     {
-        Console.WriteLine(nums[i] + " is a duplicate");
+        if (names[i] == name)
+        {
+            return true;
+        }
+
     }
-    else
-    {
-        temp.Add(nums[i]);
-    }
-}
+    return false;
+};
+//checkName();
+Console.WriteLine(checkName());
+
