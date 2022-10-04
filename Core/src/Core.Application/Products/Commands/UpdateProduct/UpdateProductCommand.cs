46d763221f654d78d7e24916889d1ddf19834137
NarminSH <nshivakhanova@gmail.com>
2022-10-04 08:01:34 +0400

    products query

--- Core/src/Core.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
+++ Core/src/Core.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using Core.Application.Common.Interfaces;
+using MediatR;
+
 namespace Core.Application.Products.Commands.UpdateProduct
 {
-    public class UpdateProductCommand
+    public class UpdateProductCommand : IRequest<int>
     {
-        public UpdateProductCommand()
+        public int Id { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateProductCommandHandler(IApplicationDbContext applicationDbContext)
         {
+            this._context = applicationDbContext;
         }
+        public async Task<int> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Products.FindAsync(new object[] { request.Id }, cancellationToken);
+            if (entity == null)
+            {
+                throw new Exception(""); //todo not found exception sinifi hazirlanacak
+            }
+            entity.ProductName = request.ProductName;
+            entity.UnitPrice = request.UnitPrice;
+            entity.UnitsInStock = request.UnitsInStock;
+            entity.CategoryId = request.CategoryId;
+
+            await _context.SaveChangesAsync(cancellationToken);
+            return default;
+        }
+
     }
 }
 
--- Core/src/Core.Application/Products/Queries/GetProducts/GetProductsQuery.cs
+++ Core/src/Core.Application/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Core.Application.Categories.Queries.GetCategories;
+using Core.Application.Common.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.Application.Products.Queries.GetProducts
 {
     public record GetProductsQuery : IRequest<IEnumerable<ProductDto>>;
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<ProductDto>>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public GetProductsQueryHandler(IApplicationDbContext applicationDbContext, IMapper mapper)
+        {
+            _context = applicationDbContext;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Products.AsNoTracking()
+                 .ProjectTo<ProductDto>(_mapper.ConfigurationProvider)
+                 .OrderBy(x => x.ProductName)
+                 .ToListAsync(cancellationToken);
+
+            return products;
+        }
+    }
 }
 
